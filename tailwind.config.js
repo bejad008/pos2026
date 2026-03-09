1c34db2c68ca45e0f0c5992d63bceb79ad093c0e
bejad008 <belajarguitar008@gmail.com>
2026-03-09 15:23:47 +0800

    Initialize Tailwind CSS configuration
    Add Tailwind CSS configuration for content paths.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,12 @@
-
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Memberitahu Tailwind di mana harus mencari class CSS yang kita gunakan
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
